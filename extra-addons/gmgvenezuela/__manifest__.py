8938c3a0178ec7411946f52550ae690fee939458
GRUPOMGCA <grupomg.com.ve@gmail.com>
2026-05-07 17:57:05 -0400

    Instalación de estructura de Libros Fiscales y activación de menús de contabilidad

---
+++ extra-addons/gmgvenezuela/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Libros Fiscales Venezuela GMG',
+    'version': '17.0.1.0.0',
+    'author': 'Grupo Mosquera Gerardo',
+    'category': 'Accounting/Localizations',
+    'summary': 'Reportes de libros de compra y venta para Venezuela',
+    'depends': [
+        'base',
+        'account',
+    ],
+    'data': ['views.xml',],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
